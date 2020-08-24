7ef0493d05532965a0d4720b20c29db84bdf2be6
JavierAcosta <javieracosta@cbit-online.com>
2020-08-24 12:39:48 -0500

    [JAVIER_ACOSTA]:creacion de proyecto admin pro

---
+++ nofound/nofound.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nofound',
+  templateUrl: './nofound.component.html',
+  styleUrls: ['./nofound.component.css']
+})
+export class NofoundComponent implements OnInit {
+
+  year  = new Date().getFullYear()
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
